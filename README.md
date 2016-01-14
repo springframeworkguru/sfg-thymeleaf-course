6b598aa19640e402125a28e694f040c9aab4ed5d
John Thompson <john@springframework.guru>
2016-01-14 16:01:33 -0500

    Intial project add

---
+++ README.md
@@ -0,0 +1,5 @@
+#Thymeleaf - Thymeleaf / Spring
+
+Source code in this repo is to support my on line course for Thymeleaf and the Spring Framework. 
+
+You can learn more about my course [here](http://courses.springframework.guru/courses/thymeleaf).
---
+++ src/main/java/guru/springframework/SfgThymeleafCourseApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgThymeleafCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgThymeleafCourseApplication.class, args);
+	}
+}
