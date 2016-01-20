f63d2623e8baee56db944c7fce75b05e0c2896db
John Thompson <john@springframework.guru>
2016-01-20 13:45:42 -0500

    updated pages to thymeleaf

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -1,6 +1,7 @@
 package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by jt on 1/20/16.
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class ProductController {
 
+    @RequestMapping("/product")
     public String getProduct(){
         return "product";
     }
