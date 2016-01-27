ddd6b64e753616680e0adaae5add0223d5fd723b
John Thompson <john@springframework.guru>
2016-01-27 16:46:22 -0500

    added dynamic content to product page

--- src/main/java/guru/springframework/controllers/ProductController.java
+++ src/main/java/guru/springframework/controllers/ProductController.java
@@ -4,6 +4,7 @@ import guru.springframework.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,13 +21,13 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/product")
+    //@RequestMapping("/product")
     public String getProduct(){
         return "redirect:/index";
     }
 
     @RequestMapping("/product/{id}")
-    public String getProductById(@RequestParam Integer id, Model model){
+    public String getProductById(@PathVariable Integer id, Model model){
 
         model.addAttribute("product", productService.getProduct(id));
 
