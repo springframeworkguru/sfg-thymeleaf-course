68e63ca84da50d55ed69319f9a8f1ae07c0817bd
John Thompson <john@springframework.guru>
2016-01-27 18:58:59 -0500

    added spring mvc config

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,6 +1,9 @@
 package guru.springframework.controllers;
 
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -9,8 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
     @RequestMapping("/")
-    public String getIndex(){
+    public String getIndex(Model model){
+
+        model.addAttribute("products", productService.listProducts());
 
         return "index";
     }
