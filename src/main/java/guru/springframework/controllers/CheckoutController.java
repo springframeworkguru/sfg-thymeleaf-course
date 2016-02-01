a31e526b1f7fc82cd2c80ff0eaf11dd3f11ed102
John Thompson <john@springframework.guru>
2016-02-01 16:54:24 -0500

    added Thymleaf Forms

--- src/main/java/guru/springframework/controllers/CheckoutController.java
+++ src/main/java/guru/springframework/controllers/CheckoutController.java
@@ -2,6 +2,7 @@ package guru.springframework.controllers;
 
 import guru.springframework.comands.CheckoutCommand;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,7 +17,10 @@ import javax.validation.Valid;
 public class CheckoutController {
 
     @RequestMapping("/checkout")
-    public String checkoutForm(){
+    public String checkoutForm(Model model){
+
+        model.addAttribute("checkoutCommand", new CheckoutCommand());
+
         return "checkoutform";
     }
 
