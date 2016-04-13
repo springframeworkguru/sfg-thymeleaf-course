5f86bb5d03eef1e7fe415bdef30d195f963d099f
John Thompson <john@springframework.guru>
2016-04-13 18:18:34 -0400

    converted to war

--- src/main/java/guru/springframework/SfgThymeleafCourseApplication.java
+++ src/main/java/guru/springframework/SfgThymeleafCourseApplication.java
@@ -2,9 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SfgThymeleafCourseApplication {
+public class SfgThymeleafCourseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SfgThymeleafCourseApplication.class, args);
