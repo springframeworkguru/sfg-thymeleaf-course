a78531586e75af9f1695673f8420b1b44d1bf596
John Thompson <john@springframework.guru>
2016-04-14 10:34:40 -0400

    adding more 'tests'

---
+++ src/test/java/guru/springframework/UnitTest.java
@@ -0,0 +1,24 @@
+package guru.springframework;
+
+import org.junit.Test;
+
+/**
+ * Created by jt on 4/14/16.
+ */
+public class UnitTest {
+
+    @Test
+    public void name() throws Exception {
+
+    }
+
+    @Test
+    public void test2() throws Exception {
+
+    }
+
+    @Test
+    public void test3() throws Exception {
+
+    }
+}
